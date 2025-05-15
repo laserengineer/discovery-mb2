0f7a79e2ef73537d8e90c2358c3e82de034a789b
Yang Yu <yy19@ualberta.ca>
2025-05-15 07:34:09 -0600

    finish reverse echo

---
+++ mdbook/src/10-uart/examples/reverse-echo.rs
@@ -0,0 +1,46 @@
+#![no_main]
+#![no_std]
+
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use heapless::Vec;
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+use microbit::{
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
+};
+
+mod serial_setup;
+use serial_setup::UartePort;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = microbit::Board::take().unwrap();
+
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, 32> = Vec::new();
+
+    loop {
+        buffer.clear();
+
+        // TODO Receive a user request. Each user request ends with ENTER
+        // NOTE `buffer.push` returns a `Result`. Handle the error by responding
+        // with an error message.
+
+        // TODO Send back the reversed string
+    }
+}
